a76af349a61695f26fba6a1d34c495202c2902c5
ecs5000@hotmail.com <ecs5000@hotmail.com>
2018-07-26 23:54:33 -0400

    jobs Models

--- porfolio/settings.py
+++ porfolio/settings.py
@@ -28,9 +28,16 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+
+
+
+
+
+
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
