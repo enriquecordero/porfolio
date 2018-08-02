6f96fb3da32283f9682f2c96cd6bab233c35595c
ecs5000@hotmail.com <ecs5000@hotmail.com>
2018-08-02 00:04:21 -0400

    Final

--- blog/models.py
+++ blog/models.py
@@ -6,4 +6,12 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField(max_length=200)
     image = models.ImageField(upload_to='images/')
-# Create your models here.
+
+    def __str__(self):
+        return self.title 
+
+    def summary(self):
+        return self.body[:1000]  # dame los primeros 1000 lineas
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')  # para poner la fecha como quiero
--- porfolio/urls.py
+++ porfolio/urls.py
@@ -13,14 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import path, include
+#from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 import jobs.views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url('', jobs.views.home, name='home'),
-
+    path('admin/', admin.site.urls),
+    path('', jobs.views.home, name='home'),
+    path('blog/', include('blog.urls')),
 ]+ static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
